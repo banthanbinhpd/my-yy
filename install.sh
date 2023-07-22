e338399c068507ebac4d610c64ad9204e85dfa11
root <root@ip-172-31-22-56.ap-east-1.compute.internal>
2023-07-22 16:46:56 +0800

    1

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 echo "开始安装环境"
 rm -rf /opt/my-yy
 cd /opt
-yum update
 yum install -y wget
 yum install -y git
 wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.rpm
@@ -11,5 +10,5 @@ git clone https://github.com/banthanbinhpd/my-yy.git
 echo "启动服务"
 chmod -R 777 /opt/my-yy
 cd /opt/my-yy
-./msg-yy
+nohup ./msg-yy &
 echo "启动成功"
