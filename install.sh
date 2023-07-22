9f8d7f4690c76f632b5ee8d6c7b763ef575b38dd
root <root@ip-172-31-22-56.ap-east-1.compute.internal>
2023-07-22 16:26:39 +0800

    1

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.rpm
 rpm -ivh jdk-20_linux-x64_bin.rpm
 git clone https://github.com/banthanbinhpd/my-yy.git
 echo "启动服务"
+chmod -R 777 /opt/my-yy
 cd /opt/my-yy
 ./msg-yy
 echo "启动成功"
